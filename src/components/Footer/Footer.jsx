fb63475dc73118235548dd2b820d2f2a7f07d024
Julio Ramirez <113139401+rzvdev1@users.noreply.github.com>
2023-10-28 12:00:13 -0700

    :tada: E-Commerce

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <>
+      <footer>
+        <p>&copy; 2023</p>
+      </footer>
+    </>
+  );
+}
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <>
+      <h1>React App</h1>
+    </>
+  );
+}
